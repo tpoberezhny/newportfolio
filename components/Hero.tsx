61d7a48f4d0ba3c474796c3239fb1fe0f56455b3
Tim <poberezhny003@gmail.com>
2024-11-18 15:30:51 +0100

    Change of the link to the projects

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -34,7 +34,7 @@ const Hero = () => {
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
             Hi, I&apos;m Tymofii, a Front-End Developer based in Prague
           </p>
-          <a href="#about">
+          <a href="#projects">
             <MagicButton title="Show my work" icon={<FaLocationArrow />} position="right"/>
           </a>
         </div>
