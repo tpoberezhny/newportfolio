d086cfda3cc8146d8a7b757e591d45119cebdb72
Tim <poberezhny003@gmail.com>
2024-11-18 15:31:10 +0100

    Adding some styling for recent project img

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -21,7 +21,11 @@ const RecentProjects = () => {
                 <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
                   <img src="/bg.png" alt="bg-img" />
                 </div>
-                <img src={img} alt={title} className="z-10 absolute bottom-0" />
+                <img
+                  src={img}
+                  alt={title}
+                  className="z-10 absolute bottom-0 w-[450px] h-[400px] rounded-lg object-cover"
+                />
               </div>
               <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
                 {title}
@@ -44,7 +48,9 @@ const RecentProjects = () => {
                   ))}
                 </div>
                 <div className="flex justify-center items-center">
-                  <p className="flex lg:text-xl md:text-xs text-sm text-purple">Check Live Site</p>
+                  <p className="flex lg:text-xl md:text-xs text-sm text-purple">
+                    Check Live Site
+                  </p>
                   <FaLocationArrow className="ms-3" color="CBACF9" />
                 </div>
               </div>
